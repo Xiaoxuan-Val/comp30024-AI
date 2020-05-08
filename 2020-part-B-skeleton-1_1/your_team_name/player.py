27d02112cf20eac81b3973e4c360f0f08277f887
bxl-0 <63153302+bxl-0@users.noreply.github.com>
2020-05-08 13:58:49 +1000

    Update player.py
    edited _init_ function

--- 2020-part-B-skeleton-1_1/your_team_name/player.py
+++ 2020-part-B-skeleton-1_1/your_team_name/player.py
@@ -1,5 +1,7 @@
 
-class ExamplePlayer:
+class Player:
+    gamestate = {}
+    colour = 'none'
     def __init__(self, colour):
         """
         This method is called once at the beginning of the game to initialise
@@ -12,7 +14,19 @@ class ExamplePlayer:
         strings "white" or "black" correspondingly.
         """
         # TODO: Set up state representation.
-
+        self.colour = colour
+        initial_white = [(0,0),(1,0),(3,0),(4,0),(6,0),(7,0),
+                         (0,1),(1,1),(3,1),(4,1),(6,1),(7,1)]
+        initial_black = [(0,6),(1,6),(3,6),(4,6),(6,6),(7,6),
+                         (0,7),(1,7),(3,7),(4,7),(6,7),(7,7)]
+        for x in range(0,7):
+            for y in range(0,7):
+                if (x,y) in initial_white:
+                    self.gamestate[(x,y)] = ['white',1]
+                elif (x,y) in initial_black:
+                    self.gamestate[(x,y)] = ['black',1]
+                else:
+                    self.gamestate[(x,y)] = ['none',0]
 
     def action(self):
         """
