528c5ba5eb3384906b894d884b0d6b43e7426efe
Xiaoxuan-Val <lixiaoxuan233@gmail.com>
2020-05-09 15:11:59 +0800

    sometimes invalid action

--- 2020-part-B-skeleton-1_1/name/player.py
+++ 2020-part-B-skeleton-1_1/name/player.py
@@ -1,55 +1,17 @@
 from random import shuffle
+from copy import deepcopy
 
 class ExamplePlayer:
     def __init__(self, colour):
-        """
-        This method is called once at the beginning of the game to initialise
-        your player. You should use this opportunity to set up your own internal
-        representation of the game state, and any other information about the 
-        game state you would like to maintain for the duration of the game.
-
-        The parameter colour will be a string representing the player your 
-        program will play as (White or Black). The value will be one of the 
-        strings "white" or "black" correspondingly.
-        """
-        # TODO: Set up state representation.
-        self.colour = colour
         
-        if colour == 'white':
-            #record self tokens in format of (x,y): token number in a dict.
-            self.tokens = {(0,0): 1, (0,1): 1, (1,0): 1, (1,1): 1,
-                           (3,0): 1, (4,0): 1, (3,1): 1, (4,1): 1,
-                           (6,0): 1, (7,0): 1, (6,1): 1, (7,1): 1}
-            
-            self.op_tokens = {(0,6): 1, (0,7): 1, (1,6): 1, (1,7): 1,
-                              (3,6): 1, (3,7): 1, (4,6): 1, (4,7): 1,
-                              (6,6): 1, (6,7): 1, (7,6): 1, (7,7): 1}
-            
-        else:
-            self.op_tokens = {(0,0): 1, (0,1): 1, (1,0): 1, (1,1): 1,
-                           (3,0): 1, (4,0): 1, (3,1): 1, (4,1): 1,
-                           (6,0): 1, (7,0): 1, (6,1): 1, (7,1): 1}
-            
-            self.tokens = {(0,6): 1, (0,7): 1, (1,6): 1, (1,7): 1,
-                              (3,6): 1, (3,7): 1, (4,6): 1, (4,7): 1,
-                              (6,6): 1, (6,7): 1, (7,6): 1, (7,7): 1}
-            
-        self.all_pos ={(x,y) for x in range(0,8) for y in range(0,8)}
-        
-        self.state = State(self.tokens, self.op_tokens)
+        self.colour = colour      
+        self.board = Board(self.colour)
+        self.state = State(self.board)
         #using magic number here for draft, will fix this later
         self.max_depth = 3
 
     def action(self):
-        """
-        This method is called at the beginning of each of your turns to request 
-        a choice of action from your program.
-
-        Based on the current state of the game, your player should select and 
-        return an allowed action to play on this turn. The action must be
-        represented based on the spec's instructions for representing actions.
-        """
-        # TODO: Decide what action to take, and return it
+        
         eval_score, selected_action = self._minimax(0, self.state, True, float('-inf'),float('inf'))
         
         return selected_action
@@ -58,39 +20,24 @@ class ExamplePlayer:
 
 
     def update(self, colour, action):
-        """
-        This method is called at the end of every turn (including your player’s 
-        turns) to inform your player about the most recent action. You should 
-        use this opportunity to maintain your internal representation of the 
-        game state and any other information about the game you are storing.
-
-        The parameter colour will be a string representing the player whose turn
-        it is (White or Black). The value will be one of the strings "white" or
-        "black" correspondingly.
-
-        The parameter action is a representation of the most recent action
-        conforming to the spec's instructions for representing actions.
-
-        You may assume that action will always correspond to an allowed action 
-        for the player colour (your method does not need to validate the action
-        against the game rules).
-        """
-        # TODO: Update state representation in response to action.
-        self.state.apply_action(action)
+        
+        self.board.update_board(action)
+        self.state.board = self.board
         
     
-    def _minimax(self, current_depth, state, is_max_turn, alpha, beta): 
+    def _minimax(self, current_depth, state, is_max_turn, alpha, beta):      
         
         action_target = ()
+        possible_actions = state.legal_actions()
+        shuffle(possible_actions)
         
         if current_depth ==self.max_depth or state.is_terminal():
+        #if current_depth ==self.max_depth:
             score = 0
             score = self.evaluation(state)
             return score, action_target
         
-        possible_actions = state.legal_actions()
         
-        shuffle(possible_actions)
         best_value = float('-inf') if is_max_turn else float('inf')
         
         
@@ -122,48 +69,41 @@ class ExamplePlayer:
     def evaluation(self,state):
         
         if state.is_terminal():
-            if state.tokens == {}:
+            if state.board.tokens == {}:
                 return 1000
             else:
                 return -1000
         
         eval_value = 0
         
-        eval_value = 100*(len(state.tokens))-100*(len(state.op_tokens))
+        eval_value = 100*(len(state.board.tokens))-100*(len(state.board.op_tokens))
         return eval_value
                 
     
-    
-     
 
-       
-   
-class State:
+class Board:
     
-    def __init__(self, tokens, op_tokens):
-            self.tokens = tokens
-            self.op_tokens = op_tokens
-            self.all_pos ={(x,y) for x in range(0,8) for y in range(0,8)}
-            
-    def legal_actions(self):
+    def __init__(self, colour): 
         
-        legal_action_list =[]
-        dist = 0
-        for position in self.tokens:
-            legal_action_list.append(("BOOM", position))
+        self.colour = colour
+        #record self tokens in format of (x,y): token number in a dict.
+        self.white_tokens = {(0,0): 1, (0,1): 1, (1,0): 1, (1,1): 1,
+                       (3,0): 1, (4,0): 1, (3,1): 1, (4,1): 1,
+                       (6,0): 1, (7,0): 1, (6,1): 1, (7,1): 1}
             
-            dist = self.tokens[position]
-            x_pos = position[0]
-            y_pos = position[1]
-            for step in range(1, dist+1):
-                for i in range(1, step+1):
-                    
-                    for pos in ((x_pos+i, y_pos),(x_pos-i, y_pos),(x_pos, y_pos+i),(x_pos, y_pos-i)):
-                        if pos in self.all_pos and pos not in self.op_tokens:
-                            legal_action_list.append(("MOVE", i, position, pos))
-        return legal_action_list
-     
-    #this function is used to count relative black token of a given position. 
+        self.black_tokens = {(0,6): 1, (0,7): 1, (1,6): 1, (1,7): 1,
+                          (3,6): 1, (3,7): 1, (4,6): 1, (4,7): 1,
+                          (6,6): 1, (6,7): 1, (7,6): 1, (7,7): 1}   
+        if colour == 'white':
+            self.tokens = self.white_tokens
+            self.op_tokens = self.black_tokens
+        else:
+            self.op_tokens = self.white_tokens
+            self.tokens = self.black_tokens
+            
+        self.all_pos ={(x,y) for x in range(0,8) for y in range(0,8)}
+        
+    #this function is used to count relative tokens of a given position. 
     #parameter point is a tuple (x, y) indicate position
     def check_surrounding(self, tokens, op_tokens, point):
         x = point[0]
@@ -205,37 +145,93 @@ class State:
                 
         return final_list
     
-    
-    
-    def apply_action(self, action):
-         
+    def update_board(self, action):
+        
         if(action[0]=="BOOM"):
+            
             remove_list =[]
             remove_list = self.check_surrounding(self.tokens, self.op_tokens, action[1])
-            
             for pos in remove_list:
                 if pos in self.tokens:
                     self.tokens.pop(pos)
                 elif pos in self.op_tokens:
                     self.op_tokens.pop(pos)
-            ###### not sure whether is is correct       
-            return State(self.tokens, self.op_tokens)
         else:
-            ##for debug
-            print()
-            print(action)
-            self.tokens[action[2]] -= action[1]
-            if (action[3] not in self.tokens.keys()):
-                self.tokens[action[3]] = action[1]
+            if action[2] in self.tokens.keys():
+                self.tokens[action[2]] -= action[1]
+                if (action[3] not in self.tokens.keys()):
+                    self.tokens[action[3]] = action[1]
+                else:
+                    self.tokens[action[3]] += action[1]
             else:
-                self.tokens[action[3]] += action[1]
+                self.op_tokens[action[2]] -= action[1]
+                if (action[3] not in self.op_tokens.keys()):
+                    self.op_tokens[action[3]] = action[1]
+                else:
+                    self.op_tokens[action[3]] += action[1]
+                
+                       
+        
+    
+   
+class State:
+    
+    def __init__(self, board):
+            self.board = board
             
-            return State(self.tokens, self.op_tokens)
+    def legal_actions(self):
+        
+        legal_action_list =[]
+        dist = 0
+        for position in self.board.tokens:
             
-                  
-       
+            legal_action_list.append(("BOOM", position))
+            
+            dist = self.board.tokens[position]
+            x_pos = position[0]
+            y_pos = position[1]
+            for step in range(1, dist+1):
+                for i in range(1, step+1):
+                    for pos in ((x_pos+i, y_pos),(x_pos-i, y_pos),(x_pos, y_pos+i),(x_pos, y_pos-i)):
+                        if pos in self.board.all_pos and pos not in self.board.op_tokens:
+                            legal_action_list.append(("MOVE", i, position, pos))
+                            
+         ##debug
+         
+        return legal_action_list
+        
+  
+    ##for evaluation in minimax algorithm
+    def apply_action(self, action):
+        
+        ##for debug
+        #print()
+        #print(action)
+        
+        new_state = deepcopy(self)
+         
+        if(action[0]=="BOOM"):
+            remove_list =[]
+            remove_list = new_state.board.check_surrounding(new_state.board.tokens, new_state.board.op_tokens, action[1])
+            
+            for pos in remove_list:
+                if pos in new_state.board.tokens:
+                    new_state.board.tokens.pop(pos)
+                elif pos in new_state.board.op_tokens:
+                    new_state.board.op_tokens.pop(pos)    
+            return new_state
+        else:
+            
+            new_state.board.tokens[action[2]] -= action[1]
+            if (action[3] not in new_state.board.tokens.keys()):
+                new_state.board.tokens[action[3]] = action[1]
+            else:
+                new_state.board.tokens[action[3]] += action[1]
+            
+            return new_state
+                
     def is_terminal(self):
-        if (self.tokens == {} or self.op_tokens == {}):
+        if (self.board.tokens == {} or self.board.op_tokens == {}):
             return True
         return False               
                 
